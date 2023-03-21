83f3ccab0d229e25710daa09b24fa5e208a167bd
zahara aviv <zahara.aviv@gmail.com>
2023-03-21 12:26:00 -0400

    M2-14 added baseline redux states and methods

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 // import './styles/index.css';
 
 const root = createRoot(document.getElementById('app') as Element);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
