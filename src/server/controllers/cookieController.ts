4c5c7f23b50ec57a8f28fbfb669e283f6a4f5840
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-03-21 20:43:42 -0400

    M2-20 start fixing userController for MySQL database

--- src/server/controllers/cookieController.ts
+++ src/server/controllers/cookieController.ts
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 import { Request, Response, NextFunction } from 'express';
+const Dotenv = require('dotenv');
 const secret = process.env.JWT_SECRET;
 
 type cookieControllerType = {
@@ -11,11 +12,16 @@ type cookieControllerType = {
 const cookieController: cookieControllerType = {
   // set session cookie with JWT
   setCookie(req, res, next) {
-    const { _id } = res.locals;
-    const sessionToken = jwt.sign({ _id }, secret);
-    res.cookie('sessionToken', sessionToken, { httpOnly: true });
-
-    return next();
+    try {
+      const { _id } = res.locals;
+      // const sessionToken = jwt.sign({ _id }, secret);
+      const sessionToken = jwt.sign({ _id }, 'secret');
+      res.cookie('sessionToken', sessionToken, { httpOnly: true });
+      return next();
+    } catch (err: any) {
+      console.log(err);
+      next(err);
+    }
   },
 
   // verify JWT from session cookie
@@ -30,21 +36,25 @@ const cookieController: cookieControllerType = {
     }
     // Decode token
     const { sessionToken } = req.cookies;
-    jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => {
-      // if verification fails, return error
-      if (err) {
-        return next({
-          log: 'Error in cookieController.verifyCookie (failed to verify token)',
-          status: 401,
-          message: { err: 'User is not authenticated.' },
-        });
+    jwt.verify(
+      sessionToken,
+      process.env.JWT_SECRET,
+      (err: Error, decoded: { _id: number }) => {
+        // if verification fails, return error
+        if (err) {
+          return next({
+            log: 'Error in cookieController.verifyCookie (failed to verify token)',
+            status: 401,
+            message: { err: 'User is not authenticated.' },
+          });
+        }
+        // if verification succeeds, add ID to res.locals and continue
+        else {
+          res.locals._id = decoded._id;
+          return next();
+        }
       }
-      // if verification succeeds, add ID to res.locals and continue
-      else {
-        res.locals._id = decoded._id;
-        return next();
-      }
-    });
+    );
   },
 
   // Clear JWT session cookie
--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -1,17 +1,17 @@
 const bcrypt = require('bcryptjs');
 import { Request, Response, NextFunction } from 'express';
-const db = require('../models/dbmodel');
+import connection from '../models/dbmodel';
 
 type userControllerType = {
   validateNewUser: (req: Request, res: Response, next: NextFunction) => void;
-  createNewUser: (req: Request, res: Response, next: NextFunction) => void;
-  authenticateUser: (req: Request, res: Response, next: NextFunction) => void;
+  // createNewUser: (req: Request, res: Response, next: NextFunction) => void;
+  // authenticateUser: (req: Request, res: Response, next: NextFunction) => void;
 };
 
 const SALT_WORK_FACTOR: number = 15;
 const userController: userControllerType = {
   validateNewUser(req, res, next) {
-    if (!req.body.email || !req.body.password) {
+    if (req.body.email === undefined || req.body.password === undefined) {
       // if missing email or password, return error
       return next({
         log: 'Error in userController.validateNewUser - email or password not provided',
@@ -22,10 +22,21 @@ const userController: userControllerType = {
       });
     } else {
       const { email } = req.body;
-      db.query('SELECT * FROM users WHERE email = $1', [email])
-        .then((result) => {
+      connection.query(
+        'SELECT * FROM users WHERE email = ?',
+        [email],
+        function (err: any, result: any, fields: any): any {
+          if (err) {
+            return next({
+              log: 'DB error while checking for duplicate email in userController.validateNewUser',
+              status: 503,
+              message: {
+                err: 'Database connection error, please try again later.',
+              },
+            });
+          }
           // if user already exists, return error
-          if (result.rows[0])
+          else if (result[0]) {
             return next({
               log: 'Error in userController.validateNewUser - user with that email already exists',
               status: 409,
@@ -33,91 +44,96 @@ const userController: userControllerType = {
                 err: 'Account with that email address already exists.',
               },
             });
+          }
           // if valid, continue to createNewUser
           else {
             return next();
           }
-        })
-        .catch((err: Error) => {
-          return next({
-            log: 'DB error while checking for duplicate email in userController.validateNewUser',
-            status: 503,
-            message: {
-              err: 'Database connection error, please try again later.',
-            },
-          });
-        });
+        }
+      );
     }
   },
-
-  createNewUser(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
-    // hash password to store in DB
-    bcrypt.hash(password, 15, (err: Error, hashedPassword: string) => {
-      // on bcrypt error, return error
-      if (err)
-        return next({
-          log: 'Error hashing password in userController.createNewUser',
-          status: 500,
-          message: {
-            err: 'Internal servor error, please try again later.',
-          },
-        });
-      // if bcrypt succeeds, create new user in database
-      db.query(
-        'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING _id',
-        [email, hashedPassword]
-      )
-        .then((result) => {
-          res.locals.userId = result.rows[0];
-          return next();
-        })
-        .catch((err: Error) => {
-          return next({
-            log: 'DB error while checking for duplicate email in userController.createNewUser',
-            status: 503,
-            message: {
-              err: 'Database connection error, please try again later.',
-            },
-          });
-        });
-    });
-  },
-
-  authenticateUser(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
-    let hashedPassword: string = '';
-    let userId: number = 0;
-    // get hashed password from DB
-    db.query('SELECT password, _id FROM users WHERE email = $1', [email])
-      .then((result) => {
-        // if valid response, save hashedPassword and userID
-        if (result.rows[0]) {
-          hashedPassword = result.rows[0].password;
-          userId = result.rows[0]._id;
-        }
-      })
-      // check if password matches
-      .then(
-        bcrypt.compare(password, hashedPassword, (isMatch: boolean) => {
-          // if yes, add user id to res.locals and continue
-          if (isMatch) {
-            res.locals._id = userId;
-            return next();
-          }
-          // otherwise, return error
-          else {
-            return next({
-              log: 'error in userController.authenticateUser: password does not match',
-              status: 401,
-              message: {
-                err: 'Incorrect email or password.',
-              },
-            });
-          }
-        })
-      );
-  },
 };
+
+//   createNewUser(req: Request, res: Response, next: NextFunction) {
+//     console.log('starting createNewUser');
+//     const { email, password } = req.body;
+//     // hash password to store in DB
+//     bcrypt.hash(password, 15, (err: Error, hashedPassword: string) => {
+//       // on bcrypt error, return error
+//       if (err)
+//         return next({
+//           log: 'Error hashing password in userController.createNewUser',
+//           status: 500,
+//           message: {
+//             err: 'Internal servor error, please try again later.',
+//           },
+//         });
+//       // if bcrypt succeeds, create new user in database
+//       connection
+//         .query(
+//           'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING _id',
+//           [email, hashedPassword]
+//         )
+//         .then((result: any) => {
+//           res.locals.userId = result.rows[0];
+//           return next();
+//         })
+//         .catch((err: Error) => {
+//           return next({
+//             log: 'DB error while checking for duplicate email in userController.createNewUser',
+//             status: 503,
+//             message: {
+//               err: 'Database connection error, please try again later.',
+//             },
+//           });
+//         });
+//     });
+//   },
+
+//   authenticateUser(req: Request, res: Response, next: NextFunction) {
+//     const { email, password } = req.body;
+//     let hashedPassword: string = '';
+//     let userId: number = 0;
+//     // get hashed password from DB
+//     connection
+//       .query('SELECT password, _id FROM users WHERE email = $1', [email])
+//       .then((result: any) => {
+//         // if valid response, save hashedPassword and userID
+//         if (result.rows[0]) {
+//           hashedPassword = result.rows[0].password;
+//           userId = result.rows[0]._id;
+//         }
+//       })
+//       // check if password matches
+//       .then(
+//         bcrypt.compare(password, hashedPassword, (isMatch: boolean) => {
+//           // if yes, add user id to res.locals and continue
+//           if (isMatch) {
+//             res.locals._id = userId;
+//             return next();
+//           }
+//           // otherwise, return error
+//           else {
+//             return next({
+//               log: 'error in userController.authenticateUser: password does not match',
+//               status: 401,
+//               message: {
+//                 err: 'Incorrect email or password.',
+//               },
+//             });
+//           }
+//         })
+//       )
+//       .catch((err: Error) => {
+//         return next({
+//           log: 'rewrite this later',
+//           status: 503,
+//           message: {
+//             err: 'Database connection error, please try again later.',
+//           },
+//         });
+//       });
+//   },
 
 module.exports = userController;
