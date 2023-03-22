a9a3a61999af79674ddab70ebef07fb22e2b9062
Evan Jones <evanjones125@gmail.com>
2023-03-22 11:48:45 -0400

    m2-16 add unit test for updating a user

--- __tests__/queryTests.js
+++ __tests__/queryTests.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2/promise');
-const { create } = require('ts-node');
 
 // testing function that takes in an SQL query string
 const testQuery = async query => {
@@ -42,6 +41,13 @@ const createUser = `
   INSERT INTO users (email, password) VALUES ('jestQueryTest@gmail.com', 'jestquerytest');
   SELECT * FROM users WHERE email='jestQueryTest@gmail.com';
 `;
+
+const updateUser = `
+  INSERT INTO users (email, password) VALUES ('wrongEmail@gmail.com', 'jestquerytest');
+  UPDATE users SET email='updatedEmail@gmail.com' WHERE email='wrongEmail@gmail.com';
+  SELECT * FROM users WHERE email='wrongEmail@gmail.com';
+  SELECT * FROM users WHERE email='updatedEmail@gmail.com';
+`;
   
 const deleteUser = `
   INSERT INTO users (email, password) VALUES ('shouldBeDeleted@gmail.com', 'shouldbedeleted');
@@ -49,31 +55,43 @@ const deleteUser = `
   SELECT * FROM users WHERE email='jestQueryTest@gmail.com';
 `;
 
-// test that our three tables exist
-it('Should find the user, applications, and actions tables', async () => {
-  const [rows] = await testQuery(`
-    ${showTables}
-  `)
+describe('Test CRUD functionality for database tables', () => {
+  // test that our three tables exist
+  it('Should find the user, applications, and actions tables', async () => {
+    const [rows] = await testQuery(`
+      ${showTables}
+    `)
 
-  expect(rows[0].Tables_in_mydb).toBe('actions');
-  expect(rows[1].Tables_in_mydb).toBe('applications');
-  expect(rows[2].Tables_in_mydb).toBe('users');
-})
+    expect(rows[0].Tables_in_mydb).toBe('actions');
+    expect(rows[1].Tables_in_mydb).toBe('applications');
+    expect(rows[2].Tables_in_mydb).toBe('users');
+  })
 
-// test that we can create a user
-it('Should create a new user in users', async () => {
-  const [rows] = await testQuery(`
-    ${createUser}
-  `)
+  // test that we can create a user
+  it('Should create a new user in users', async () => {
+    const [rows] = await testQuery(`
+      ${createUser}
+    `)
 
-  expect(rows[1][0]).toBeTruthy();
-})
+    expect(rows[1][0]).toBeTruthy();
+  })
 
-// test that we can delete a user
-it('Should be able to delete a user in the users table', async () => {
-  const [rows] = await testQuery(`
-    ${deleteUser}
-  `)
-  
-  expect(rows[1][0]).not.toBeTruthy();
+  // test that we can update a user
+  it('Should be able to update a user in the users table', async () => {
+    const [rows] = await testQuery(`
+      ${updateUser}
+    `)
+    
+    expect(rows[2][0]).not.toBeTruthy();
+    expect(rows[3][0]).toBeTruthy();
+  })
+
+  // test that we can delete a user
+  it('Should be able to delete a user in the users table', async () => {
+    const [rows] = await testQuery(`
+      ${deleteUser}
+    `)
+    
+    expect(rows[1][0]).not.toBeTruthy();
+  })
 })
