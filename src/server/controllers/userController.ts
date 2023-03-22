2d06f310ce9ddc28126255f7e85b4ba96a299400
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-03-22 10:47:19 -0400

    M2-20 rewrite createNewUser and authenticateUser for MySQL queries

--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -4,8 +4,8 @@ import connection from '../models/dbmodel';
 
 type userControllerType = {
   validateNewUser: (req: Request, res: Response, next: NextFunction) => void;
-  // createNewUser: (req: Request, res: Response, next: NextFunction) => void;
-  // authenticateUser: (req: Request, res: Response, next: NextFunction) => void;
+  createNewUser: (req: Request, res: Response, next: NextFunction) => void;
+  authenticateUser: (req: Request, res: Response, next: NextFunction) => void;
 };
 
 const SALT_WORK_FACTOR: number = 15;
@@ -53,87 +53,88 @@ const userController: userControllerType = {
       );
     }
   },
+
+  createNewUser(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    // hash password to store in DB
+    bcrypt.hash(password, 15, (err: Error, hashedPassword: string) => {
+      // on bcrypt error, return error
+      if (err)
+        return next({
+          log: 'Error hashing password in userController.createNewUser',
+          status: 500,
+          message: {
+            err: 'Internal servor error, please try again later.',
+          },
+        });
+      // if bcrypt succeeds, create new user in database
+      connection.query(
+        'INSERT INTO users (email, password) VALUES (?, ?) RETURNING _id',
+        [email, password],
+        function (err: any, result: any, fields: any): any {
+          // return DB error if query fails
+          if (err) {
+            return next({
+              log: 'DB error while checking for duplicate email in userController.createNewUser',
+              status: 503,
+              message: {
+                err: 'Database connection error, please try again later.',
+              },
+            });
+          }
+          // if query succeeds, assign id to res.locals and continue
+          res.locals.userId = result[0]._id;
+          return next();
+        }
+      );
+    });
+  },
+
+  authenticateUser(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    let hashedPassword: string = '';
+    let userId: number = 0;
+    // get hashed password from DB
+    connection.query(
+      'SELECT password, _id FROM users WHERE email = ?',
+      [email],
+      function (err: any, result: any, fields: any): any {
+        // return DB error if query fails
+        if (err) {
+          return next({
+            log: 'DB error while querying in userController.authenticateUser',
+            status: 503,
+            message: {
+              err: 'Database connection error, please try again later.',
+            },
+          });
+        }
+        // if valid response, save hashedPassword and userID
+        if (result[0]) {
+          hashedPassword = result[0].password;
+          userId = result.rows[0]._id;
+        }
+        // check if password matches
+        bcrypt.compare(password, hashedPassword, (isMatch: boolean) => {
+          // if yes, add user id to res.locals and continue
+          if (isMatch) {
+            res.locals._id = userId;
+            return next();
+          }
+          // otherwise, return error
+          else {
+            return next({
+              log: 'error in userController.authenticateUser: email or password does not match',
+              status: 401,
+              message: {
+                err: 'Incorrect email or password.',
+              },
+            });
+          }
+        });
+      }
+    );
+  },
 };
-
-//   createNewUser(req: Request, res: Response, next: NextFunction) {
-//     console.log('starting createNewUser');
-//     const { email, password } = req.body;
-//     // hash password to store in DB
-//     bcrypt.hash(password, 15, (err: Error, hashedPassword: string) => {
-//       // on bcrypt error, return error
-//       if (err)
-//         return next({
-//           log: 'Error hashing password in userController.createNewUser',
-//           status: 500,
-//           message: {
-//             err: 'Internal servor error, please try again later.',
-//           },
-//         });
-//       // if bcrypt succeeds, create new user in database
-//       connection
-//         .query(
-//           'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING _id',
-//           [email, hashedPassword]
-//         )
-//         .then((result: any) => {
-//           res.locals.userId = result.rows[0];
-//           return next();
-//         })
-//         .catch((err: Error) => {
-//           return next({
-//             log: 'DB error while checking for duplicate email in userController.createNewUser',
-//             status: 503,
-//             message: {
-//               err: 'Database connection error, please try again later.',
-//             },
-//           });
-//         });
-//     });
-//   },
-
-//   authenticateUser(req: Request, res: Response, next: NextFunction) {
-//     const { email, password } = req.body;
-//     let hashedPassword: string = '';
-//     let userId: number = 0;
-//     // get hashed password from DB
-//     connection
-//       .query('SELECT password, _id FROM users WHERE email = $1', [email])
-//       .then((result: any) => {
-//         // if valid response, save hashedPassword and userID
-//         if (result.rows[0]) {
-//           hashedPassword = result.rows[0].password;
-//           userId = result.rows[0]._id;
-//         }
-//       })
-//       // check if password matches
-//       .then(
-//         bcrypt.compare(password, hashedPassword, (isMatch: boolean) => {
-//           // if yes, add user id to res.locals and continue
-//           if (isMatch) {
-//             res.locals._id = userId;
-//             return next();
-//           }
-//           // otherwise, return error
-//           else {
-//             return next({
-//               log: 'error in userController.authenticateUser: password does not match',
-//               status: 401,
-//               message: {
-//                 err: 'Incorrect email or password.',
-//               },
-//             });
-//           }
-//         })
-//       )
-//       .catch((err: Error) => {
-//         return next({
-//           log: 'rewrite this later',
-//           status: 503,
-//           message: {
-//             err: 'Database connection error, please try again later.',
-//           },
-//         });
-//       });
-//   },
 
 module.exports = userController;
