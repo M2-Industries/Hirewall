7f8c0e81e6977f55209a22e75456afc890bded62
Evan Jones <evanjones125@gmail.com>
2023-03-22 14:16:39 -0400

    m2-16 remove an uneccessary test from userTests

--- __tests__/userTests.js
+++ __tests__/userTests.js
@@ -3,22 +3,6 @@ const server = 'http://localhost:3000';
 const mysql = require('mysql2/promise');
 
 describe('Route integration', () => {
-  xdescribe('GET /user', function() {
-    it('responds with an array of user objects', function(done) {
-      request(server)
-        .get('/user')
-        .expect(200)
-        .then((response) => {
-          const body = response._body;
-          expect(Array.isArray(body)).toBe(true);
-          expect(body[0]._id).toBeTruthy();
-          expect(body[0].email).toBeTruthy();
-          expect(body[0].password).toBeTruthy();
-        })
-        .then(done);
-    });
-  });
-
   describe('POST /user/create', function() {   
     const pool = mysql.createPool({
       host: 'db-hirewall.cr6amg22ob1r.us-west-1.rds.amazonaws.com',
